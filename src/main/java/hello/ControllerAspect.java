71ef7285c4a9635d2c3787df2ee009594efc4d49
yuhayakawa <gittest@git.com>
2018-03-30 02:20:55 +0900

    moromoro

---
+++ src/main/java/hello/ControllerAspect.java
@@ -0,0 +1,13 @@
+//package hello;
+//
+//@Aspect
+//public class ControllerAspect {
+//
+//	@Around("execution(* gagaga(**))")
+//	public Object around(ProceedingJoinPoint point) throws Throwable {
+//
+//		Object obj = point.proceed();
+//
+//		return obj;
+//	}
+//}
--- src/main/java/hello/ProductService.java
+++ src/main/java/hello/ProductService.java
@@ -5,9 +5,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProductService {
 
+//	private static ProductService productService = new ProductService();
+
+	public ProductService() {
+		System.out.println("インスタンス生成したお");
+	}
+
+
 	public void gagaga() {
 
-		System.out.println("(*'▽')");
+		System.out.println("( ^^) _旦~~");
 	}
 
 }
