d5a63a2bee27ad5ad817e2ac8b252b57bab962e3
yuhayakawa <gittest@git.com>
2018-03-30 01:51:02 +0900

    di

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +9,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
+	@Autowired
+	private ProductService productService;
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="doumo", required=false, defaultValue="World") String doumo, Model model) {
+
+    	System.out.println("start");
+
+    	productService.gagaga();
+
         model.addAttribute("doumo", doumo);
+
+        System.out.println("end");
+
         return "greeting";
     }
-
 }
---
+++ src/main/java/hello/ProductService.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+	public void gagaga() {
+
+		System.out.println("(*'▽')");
+	}
+
+}
